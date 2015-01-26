1141e1315e116d42d6c668f0e977a5c7f4d95350
Martin <martinochwat1@gmail.com>
2015-01-26 16:57:29 -0500

    Updated

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,11 +1,6 @@
 require 'jumpstart_auth'
 require 'bitly'
 
-Bitly.use_api_version_3
-
-bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
-puts bitly.shorten('http://jumpstartlab.com/courses/').short_url
-
 class MicroBlogger
 	attr_reader :client
 
@@ -35,7 +30,6 @@ class MicroBlogger
 			when 'dm' then dm(parts[1], parts[2..-1].join(" "))
 			when 'spam' then span_my_followers(parts[1..-1].join(" "))
 			when 'elt' then everyones_last_tweet
-			when 's' then shorten(parts[1])
 			when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 			when 'q' then puts "Goodbye!"
 			else 
